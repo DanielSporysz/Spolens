32471e760404f6ad05f574a05df2b635409077b6
DanielSporysz <daniel_spory@o2.pl>
2020-03-20 17:05:09 +0100

    casting 3d to 2d test

--- Spolens/cfgreader.py
+++ Spolens/cfgreader.py
@@ -1,6 +1,13 @@
 import re
 
 
+def cast_coordinates_to_float(coordinates):
+    casted = []
+    for element in coordinates:
+        casted.append(float(element)/10)
+    return casted
+
+
 def load_objects_configuration(file_name):
     f = open(file_name, "r")
     if f.mode == 'r':
@@ -8,14 +15,16 @@ def load_objects_configuration(file_name):
 
         for idx, file_line in enumerate(f.readlines()):
             if file_line[0] != '#':
-                points = re.findall(r'\d+ \d+ \d+', file_line)
+                points = re.findall(r'-?\d+ -?\d+ -?\d+', file_line)
 
                 if len(points) != 2:
                     raise Exception(
                         "Line " + idx + " has unsopported data format!")
 
-                start_point = re.findall(r'\d+', points[0])
-                end_point = re.findall(r'\d+', points[1])
+                start_point = cast_coordinates_to_float(
+                    re.findall(r'-?\d+', points[0]))
+                end_point = cast_coordinates_to_float(
+                    re.findall(r'-?\d+', points[1]))
 
                 lines.append([start_point, end_point])
 
--- Spolens/spolens.py
+++ Spolens/spolens.py
@@ -1,20 +1,50 @@
 import pyglet
 from pyglet.gl import *
-from cfreader import load_objects_configuration
+from cfgreader import load_objects_configuration
+import numpy as np
 
 lines = load_objects_configuration("lines.txt")
 
-win = pyglet.window.Window(1024, 576)
+win = pyglet.window.Window(600, 600)
+
 
 def draw_line(start, end):
     glBegin(GL_LINES)
-    glVertex3f(float(start[0]),float(start[1]),float(start[2]))
-    glVertex3f(float(end[0]),float(end[1]),float(end[2]))
+    glVertex3f(start[0], start[1], 1)
+    glVertex3f(end[0], end[1], 1)
     glEnd()
+
+
+def cast_on_screen(lines):
+    lines_on_screen = []
+
+    for line in lines:
+        line_points_on_screen = []
+        for point in line:
+            # coefficient = 2/point[2]
+            cast_matrix = np.array([
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1/point[2], 0]
+            ])
+            point.append(1)
+            new_point = cast_matrix.dot(point)
+            line_points_on_screen.append(new_point)
+
+        lines_on_screen.append(
+            [line_points_on_screen[0], line_points_on_screen[1]])
+
+    return lines_on_screen
+
 
 @win.event
 def on_draw():
-    for line in lines:
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    lines_on_screen = cast_on_screen(lines)
+    for line in lines_on_screen:
         draw_line(line[0], line[1])
+
 
 pyglet.app.run()
