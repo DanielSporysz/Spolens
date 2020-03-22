d4a72a5401d5b9f708df414b0a645bd9ad00a707
DanielSporysz <daniel_spory@o2.pl>
2020-03-22 14:42:25 +0100

    zoom, better controls, more labels

--- Spolens/window.py
+++ Spolens/window.py
@@ -6,6 +6,7 @@ from structures import Line
 from calc import *
 
 
+
 class SpolensWindow(pyglet.window.Window):
     def __init__(self, width, height, title, distance_to_screen, lines):
         self.distance_to_screen = distance_to_screen
@@ -14,6 +15,7 @@ class SpolensWindow(pyglet.window.Window):
         self.step = 10
         self.slow_step = 2
         self.r_step = 1
+        self.d_step = 10
 
         self.font_size = 20
         self.controls_labelWS = pyglet.text.Label(
@@ -23,11 +25,13 @@ class SpolensWindow(pyglet.window.Window):
         self.controls_labelQE = pyglet.text.Label(
             'QE - OZ', x=0, y=height-3*self.font_size)
         self.controls_labelTG = pyglet.text.Label(
-            'TG - rOY', x=0, y=height-4*self.font_size)
+            'FH - rOY', x=0, y=height-4*self.font_size)
         self.controls_labelFH = pyglet.text.Label(
-            'FH - rOX', x=0, y=height-5*self.font_size)
+            'TG - rOX', x=0, y=height-5*self.font_size)
         self.controls_labelRY = pyglet.text.Label(
             'RY - rOZ', x=0, y=height-6*self.font_size)
+        self.controls_labelZoom = pyglet.text.Label(
+            'ZX - rOZ', x=0, y=height-7*self.font_size)
 
         super().__init__(width, height, title)
 
@@ -39,12 +43,18 @@ class SpolensWindow(pyglet.window.Window):
         for line in screen_lines:
             self.draw_line(line.start, line.end, line.color)
 
+        # display controls help
         self.controls_labelAD.draw()
         self.controls_labelWS.draw()
         self.controls_labelQE.draw()
         self.controls_labelTG.draw()
         self.controls_labelFH.draw()
         self.controls_labelRY.draw()
+        self.controls_labelZoom.draw()
+
+        # display parameters
+        pyglet.text.Label(
+            'd = ' + str(self.distance_to_screen), x=0, y=0).draw()
 
     def on_text(self, text):
         # OX
@@ -63,20 +73,27 @@ class SpolensWindow(pyglet.window.Window):
         elif text == 'e':
             self.lines = translate_lines(self.lines, 0, 0, -self.slow_step)
         # rOX
-        elif text == 'f':
-            self.lines = rotate_lines(self.lines, self.r_step, "x")
-        elif text == 'h':
-            self.lines = rotate_lines(self.lines, -self.r_step, "x")
-        # rOY
         elif text == 't':
-            self.lines = rotate_lines(self.lines, self.r_step, "y")
+            self.lines = rotate_lines(self.lines, -self.r_step, "x")
         elif text == 'g':
+            self.lines = rotate_lines(self.lines, self.r_step, "x")
+        # rOY
+        elif text == 'f':
             self.lines = rotate_lines(self.lines, -self.r_step, "y")
+        elif text == 'h':
+            self.lines = rotate_lines(self.lines, self.r_step, "y")
         # rOY
         elif text == 'r':
             self.lines = rotate_lines(self.lines, self.r_step, "z")
         elif text == 'y':
             self.lines = rotate_lines(self.lines, -self.r_step, "z")
+        # zoom
+        elif text == 'z':
+            self.distance_to_screen -= self.d_step
+            if self.distance_to_screen < 10:
+                self.distance_to_screen = 10
+        elif text == 'x':
+            self.distance_to_screen += self.d_step
 
         self.on_draw()
 
